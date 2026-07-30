afe3c9c332b9d73dd75c95622b173b5b4bb23fd0
lingtingjimozdb <lingtingjimozdb@gmail.com>
2026-07-30 11:28:28 +0800

    Refresh PWA cache for V9.1 AI restore

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="paperbook-cloud-v9";
+const CACHE="paperbook-cloud-v9-1-ai-restore";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./og.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(
